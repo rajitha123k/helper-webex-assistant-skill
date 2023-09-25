c572839d5ec0cbb6cf44257d07601b916eb1ec59
Rajitha Kantheti <rajithakantheti@Rajithas-Mac-mini.local>
2023-09-24 20:19:13 -0400

    project code

--- helper-skill/helper/main.py
+++ helper-skill/helper/main.py
@@ -4,7 +4,9 @@ from webex_skills.models.mindmeld import DialogueState
 import requests
 import json
 import os
+import requests
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,7 +19,7 @@ async def headlines(current_state: DialogueState) -> DialogueState:
     new_state = current_state.copy()
 
     text = 'Ok, showing todays headlines.'
-    url = os.getenv('NEW_API_URL')+"/top-headlines?country=us&apiKey="+os.getenv('NEWS_API_KEY')
+    url = os.getenv('NEWS_API_URL')+"/top-headlines?country=us&apiKey="+os.getenv('NEWS_API_KEY')
 
     response = requests.get(url)
 
@@ -63,7 +65,7 @@ async def weather(current_state: DialogueState,  query: str) -> DialogueState:
     with open(file_path, 'r') as f:
         data = json.load(f)
     
-    if res == "":
+    if res == "" or query.find("in")== -1:
         text="Please repeat the command with city name in it."
     else:
         for x in data:
@@ -78,8 +80,8 @@ async def weather(current_state: DialogueState,  query: str) -> DialogueState:
 
         response_json = response.json()
         print(response_json)
-        if response_json is None:
-            text= 'Sorry, I was not able to find the weather for the mentioned city. Please try again!'
+        # if response_json['cod']=='400':
+        #     text= 'Sorry, I was not able to find the weather for the mentioned city. Please try again!'
         text = 'Its '+str(response_json['temp'])+' degrees and '+str(response_json['description'])+' in '+str(response_json['place'])+' today.'
 
     # assistant_event_payload = {
@@ -103,14 +105,71 @@ async def weather(current_state: DialogueState,  query: str) -> DialogueState:
 
 @api.handle(pattern=r'.*\smeetings\s?.*')
 async def meetings(current_state: DialogueState,  query: str) -> DialogueState:
-    print(current_state)
+    # print(current_state)
     new_state = current_state.copy()
+    newer_state=current_state.copy()
+    print(new_state.context['developerDeviceId'])
+
+    url = 'https://webexapis.com/v1/xapi/command/Bookings.List'
+    myobj = {
+    "deviceId": new_state.context['developerDeviceId'],
+    "arguments": {
+    "DayOffset": 0,
+    "Days": 1,
+    "Limit": 200,
+    "Offset": 0
+    }
+    }
+
+    x = requests.post(url, headers={'Content-Type':'application/json',
+               'Authorization': 'Bearer '+os.getenv('WEBEX_ACCESS_TOKEN')}, json = myobj)
+
+    print(x.json())
     
-    text = 'Ok, showing todays headlines.'
+
+    text="you have "+str(len(x.json()['result']['Booking']))+' meetings '
+    
+    bookText=""
+    meetings=[]
+    for booking in x.json()['result']['Booking']:
+        
+        print(booking['Id'])
+        url = 'https://webexapis.com/v1/xapi/command/Bookings.Get'
+        myobj = {
+        "deviceId": new_state.context['developerDeviceId'],
+        "arguments": {
+        "Id": booking['Id']
+        }
+        }
+
+        resp = requests.post(url, headers={'Content-Type':'application/json',
+                'Authorization': 'Bearer '+os.getenv('WEBEX_ACCESS_TOKEN')}, json = myobj)
+        print(resp.json()['result']['Booking']['Title'])
+        time=resp.json()['result']['Booking']['Time']['StartTime']
+        res=time[time.find('T')+len('T'):-4]
+        t_obj = datetime.strptime( res, '%H:%M')
+        t_am_pm = t_obj.strftime('%I:%M %p')
+        print(t_am_pm)
+        bookText=bookText+resp.json()['result']['Booking']['Title']+ " at "
+        bookText=bookText+t_am_pm+", \n"
+        meetings.append(resp.json()['result']['Booking']['Title']+ " at "+t_am_pm)
+    
+    assistant_event_payload = {
+        'name': 'Meetings',
+        'payload': {
+            'text': meetings,
+        },     
+    }
+    
+    # text = 'Ok, showing todays meetings.'
     new_state.directives = [
         responses.Reply(text),
         responses.Speak(text),
-        responses.DisplayWebView(os.getenv('WEBVIEW_URL'),"Cisco"),
+        responses.AssistantEvent(payload=assistant_event_payload),
+        responses.Reply(bookText),
+        responses.Speak(bookText),
+        
+        # responses.DisplayWebView(os.getenv('WEBVIEW_URL'),"Cisco"),
         responses.Sleep(10),
     ]
 
